f391bd278228d670db0a773b9bb2eb20332c0a9d
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-05-08 09:51:28 +0200

    Update 14all.js

--- js/14all.js
+++ js/14all.js
@@ -1,2 +1,9 @@
 "use strict";
 const $=(y)=>document.querySelector(y);
+
+function getURLParameter(name) {
+		return decodeURIComponent(
+			(location.search.match(RegExp("[?|&]"+name+'=(.+?)(&|$)'))||[,null])[1]
+		);
+}
+
