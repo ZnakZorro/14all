68a7617a722a14e78d49b6a88b0b02faf5b26f47
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-05-18 12:25:06 +0200

    Update 14all.js

--- js/14all.js
+++ js/14all.js
@@ -12,7 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	let menu14=["home","pogoda","radio","todo","3d","links"];
 	let menu_place = $('nav');
 	console.log('place=',menu_place);
-	let path = location.pathname.split('/').pop().pop();
+	let arrpath = location.pathname.split('/');
+	let path = arrpath[arrpath.length-1];
 	console.log('path=',path);
 	menu14.forEach(function(m){
 		console.log(m);	
