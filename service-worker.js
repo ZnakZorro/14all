ecc9f95cba04734104f2e4a1d190ca1c5f7327f9
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-05-19 07:43:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 "use strict";
-const cachNR = 19;
+const cachNR = 20;
 var PRECACHE = 'pre_14all_'+(cachNR+0);
 var RUNTIME  = 'off_14all_'+(cachNR+1);
 //console.log(PRECACHE,RUNTIME);
@@ -15,16 +15,17 @@ const PRECACHE_URLS = [
 	"/14all/css/mdb.min.css",
 	"/14all/css/home.svg",
 	"/14all/strony/3d/models/altanka.dae",
+	"/14all/strony/3d/models/kosiarka.dae",
 	"/14all/strony/3d/models/Skrzynia.dae",
 	"/14all/strony/3d/models/Grill.dae",
-	"./img/3d.png",
-	"./img/3d-32.png",
-	"./img/3d-72.png",
-	"./img/3d-96.png",	
-	"./img/3d-128.png",
-	"./img/3d-144.png",
-	"./img/3d-256.png",
-	"./img/3d-512.png",
+	"/14all/img/3d.png",
+	"/14all/img/3d-32.png",
+	"/14all/img/3d-72.png",
+	"/14all/img/3d-96.png",	
+	"/14all/img/3d-128.png",
+	"/14all/img/3d-144.png",
+	"/14all/img/3d-256.png",
+	"/14all/img/3d-512.png",
 	"https://fonts.googleapis.com/css?family=Roboto:100,300,400,400i,900,900i&amp;subset=latin-ext"
 	
 ];
