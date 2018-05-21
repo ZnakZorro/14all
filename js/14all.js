d3ef79e932f0a45b50660bc4615679c6154dbde5
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-05-21 12:54:04 +0200

    Update 14all.js

--- js/14all.js
+++ js/14all.js
@@ -32,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	});
 	//console.log(newDiv);
 	//$('.mobile-wrap').appendChild(newDiv);
-	$('.nav').appendChild(newDiv);
+	//$('.nav').appendChild(newDiv);
+	$('.nav').innerHTML = menuHtml;
 });
 
