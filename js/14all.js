2c300c6af6309c9f68ea574ad185b82bf54db17b
Znak Zorro <ZnakZorro@users.noreply.github.com>
2018-05-21 13:04:16 +0200

    Update 14all.js

--- js/14all.js
+++ js/14all.js
@@ -9,26 +9,22 @@ function getURLParameter(name) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	let menu14=["home","pogoda","radio","todo","3d","links"];
+	
 	let xxyy = $('.content').getBoundingClientRect();
 	let hh = window.innerHeight;	
-	console.log(xxyy);
 	$('.content').style.height = (hh - xxyy.top + 10)+'px';
-	
-	let menu14=["home","pogoda","radio","todo","3d","links"];
-	//let menu_place = $('div.nav');
-	//console.log('place=',menu_place);
 	let arrpath = location.pathname.split('/');
 	console.log('arrpath=',arrpath);
 	let path = arrpath[arrpath.length-2];
 	console.log('path=',path);
 	
 	let menuHtml ='';
-	//let newDiv = document.createElement('div');
-	//newDiv.className="nav";
 	menu14.forEach(function(m){
 		console.log(m);	
-		//newDiv.innerHTML += m+'<br';
-		menuHtml += '<a href="./'+m+'" class="nava icon-'+m+'" title="'+m.toUpperCase()+'"><img src="/14all/css/'+m+'.svg" class="maska"></a>';
+		let pre = '../../';
+		if (path == '14all') pre='./';
+		menuHtml += '<a href="'+pre+m+'" class="nava icon-'+m+'" title="'+m.toUpperCase()+'"><img src="/14all/css/'+m+'.svg" class="maska"></a>';
 	});
 	//console.log(newDiv);
 	//$('.mobile-wrap').appendChild(newDiv);
